e6d7d779bcead0014187ecf69a812e202dbefe40
Mark Melnic <markhappy200@gmail.com>
2020-09-14 10:23:55 +0200

    Fixed tests

--- tests/test_score.py
+++ tests/test_score.py
@@ -1,19 +1,19 @@
 
 import unittest
-import scalg
+from scalg.scalg import score
 
 class Score(unittest.TestCase):
-    self.data = [2016 ,21999 ,62000  ,181,
-                2013 ,21540 ,89000  ,223,
-                2015 ,18900 ,100000 ,223,
-                2013 ,24200 ,115527 ,223,
-                2016 ,24990 ,47300  ,223]
+    data = [[2016 ,21999 ,62000  ,181],
+                [2013 ,21540 ,89000  ,223],
+                [2015 ,18900 ,100000 ,223],
+                [2013 ,24200 ,115527 ,223],
+                [2016 ,24990 ,47300  ,223]]
 
     def test_score_default(self):
-        scalg.score(self.data, [1, 0, 0, 1])
+        score(self.data, [1, 0, 0, 1])
 
     def test_score_scores(self):
-        scalg.score(self.data, [1, 0, 0, 1], 'scores')
+        score(self.data, [1, 0, 0, 1], 'scores')
 
     def test_score_score_lists(self):
-        scalg.score(self.data, [1, 0, 0, 1], 'score_lists')
+        score(self.data, [1, 0, 0, 1], 'score_lists')
